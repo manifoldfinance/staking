bdef10252d709f042d5c601cadbff49a292ef459
sam bacha <sam@manifoldfinance.com>
2021-12-22 04:40:30 -0800

    build(nexjs): update config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,31 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   productionBrowserSourceMaps: false,
   poweredByHeader: false,
   reactStrictMode: true,
   optimizeImages: true,
   optimizeCss: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+  experimental: { 
+    esmExternals: true,
+    outputFileTracing: true
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
   },
-});
+  images: {
+    minimumCacheTTL: 1209600,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['raw.githubusercontent.com'],
+  }
+}
 
+
+module.exports = (nextConfig)
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+/** @exports nextConfig */
