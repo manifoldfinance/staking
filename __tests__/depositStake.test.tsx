7eb216adb950abcb9d5bccd3c99e95bdda2e2beb
NtwaliHeritier <heritierntwali@gmail.com>
2021-12-31 18:17:46 +0200

    add ability to fork the mainnet

--- __tests__/depositStake.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import DepositStake from "@/components/stake/depositStake"
-
-describe("DepositStake", () => {
-    it("checks if there is a submit button", () => {
-        const {getByText} = render(<DepositStake/>);
-        expect(getByText('Permit FOLD to mint xFOLD')).toBeInTheDocument();
-    })
-})
