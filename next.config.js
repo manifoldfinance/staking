f567dd01aa376cc5fc0871d038d3605722be1844
sam bacha <sam@manifoldfinance.com>
2022-01-01 06:54:42 -0800

    feat(react): zustand

--- next.config.js
+++ next.config.js
@@ -5,5 +5,27 @@ module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
   optimizeImages: true,
-  optimizeCss: true
+  optimizeCss: true,
+  experimental: { 
+    esmExternals: true,
+    outputFileTracing: true
+  },
+  images: {
+    domains: [
+      'raw.githubusercontent.com',
+      'assets.coingecko.com',
+      'logos.covalenthq.com',
+      'www.covalenthq.com',
+      's2.coinmarketcap.com',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    return config
+  },
 }
+
+// Don't delete this console log, useful to see the config in Vercel deployments
+console.log('next.config.js', JSON.stringify(module.exports, null, 2))
