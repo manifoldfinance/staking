6f1b7c6783f7f53b30d073ba87eb15d9fc9e6852
Sasidharan K <me@sasi.codes>
2022-02-10 05:27:54 +0000

    feat: added ens avatar support

--- components/identicon.tsx
+++ components/identicon.tsx
@@ -1,16 +1,5 @@
-import jazzicon from 'jazzicon-ts';
-import { useEffect, useRef } from 'react';
+import Davatar from '@davatar/react';
 
 export default function Identicon({ address }: { address: string }) {
-  const ref = useRef<HTMLDivElement>();
-
-  useEffect(() => {
-    if (address && ref.current) {
-      ref.current.innerHTML = '';
-
-      ref.current.appendChild(jazzicon(20, parseInt(address.slice(2, 10), 16)));
-    }
-  }, [address]);
-
-  return <div className="h-5 w-5 rounded-full bg-primary-400" ref={ref} />;
+  return <Davatar size={20} address={address} generatedAvatarType="jazzicon" />;
 }
