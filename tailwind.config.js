60941bd19b83ce14a13be07db0f8f8caf493c421
sam bacha <sam@manifoldfinance.com>
2022-08-09 07:52:01 -0700

    fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,67 @@
+// @ts-check
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+// const defaultTheme = require('tailwindcss/defaultTheme')
+// const plugin = require('tailwindcss/plugin')
+
+const height = require('./height');
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './components/**/*.{tsx,ts}',
-    './pages/**/*.{tsx,ts}',
+  content: [
+    //    './pages/**/*.{js,ts,jsx,tsx}',
+    //    './components/**/*.{js,ts,jsx,tsx}',
     './views/**/*.{tsx,ts}',
     './constants/*.ts',
+    './components/**/*.{tsx,ts}',
+    './pages/**/*.{tsx,ts}',
+//    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+//    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+//    ...createGlobPatternsForDependencies(__dirname),
   ],
-  darkMode: 'media',
+  darkMode: 'class',
   theme: {
     extend: {
+      scale: {
+        102: '1.02',
+      },
+      height,
+      minHeight: height,
+      borderWidth: {
+        1: '1px',
+      },
       colors: {
-        primary: {
-          DEFAULT: '#0C0F21',
-          400: '#181b2c',
-          300: '#303342',
+        bg: {
+          light: '#f4f4f5',
+          dark: '#262626',
         },
-        network: {
-          rinkeby: '#f6c343',
+        font: {
+          light: '#1c1917',
+          dark: '#fafafa',
+        },
+        searchbar: {
+          light: '#d4d4d8',
+          dark: '#71717a',
+        },
+        primary: {
+          light: '#333333',
+        },
+        secondary: {
+          light: '#87878a',
+        },
+        tertiary: {
+          light: '#a3a4a8',
+        },
+        link: {
+          light: '#001d82',
+        },
+        inset: {
+          light: '#dddcda',
+        },
+        success: {
+          light: '#2aba13',
         },
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
