dcc2b1c6b0a9f170a14dff44af03c00b824e45cc
maVexxe <mavexxe1@gmail.com>
2022-12-08 00:40:11 +0100

    upgrade: reduce rerenders

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,11 +1,41 @@
-import OperationManager from "./UserOperationManager"
+import OperationManager from "./UserOperationManager";
+import { Tab } from "@headlessui/react";
+import { useState, createContext } from "react";
 
+interface ISelectedTabContext {
+  SelectedTab: number;
+  setSelectedTab: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export const SelectedTabContext = createContext<ISelectedTabContext>(null!);
 
 export default function Tabs() {
+  const [SelectedTab, setSelectedTab] = useState(0);
   return (
     <section className="py-[130px] px-2 mx-auto max-w-[485px]">
       <div className="flex flex-col gap-2 p-2 md:p-4 rounded-[28px] bg-dark-800 shadow-md relative w-full max-w-xl bg-grayish ">
-        <OperationManager />
+        <Tab.Group selectedIndex={SelectedTab} onChange={setSelectedTab}>
+          <Tab.List className="p-1 grid grid-cols-2 gap-4 items-center pb-2 text-white ">
+            <Tab className=" text-slate-300 text-lg font-light cursor-pointer hover:bg-fresh focus:outline-none aria-selected:bg-fresh rounded-[14px] border border-fresh p-3">
+              Stake
+            </Tab>
+            <Tab className="text-slate-300 text-lg font-light cursor-pointer hover:bg-fresh focus:outline-none aria-selected:bg-fresh rounded-[14px] border border-fresh p-3">
+              Withdraw
+            </Tab>
+          </Tab.List>
+          <Tab.Panels className="p-2">
+            <SelectedTabContext.Provider
+              value={{ SelectedTab, setSelectedTab }}
+            >
+              <Tab.Panel>
+                <OperationManager />
+              </Tab.Panel>
+              <Tab.Panel>
+                <OperationManager />
+              </Tab.Panel>
+            </SelectedTabContext.Provider>
+          </Tab.Panels>
+        </Tab.Group>
       </div>
     </section>
   );
--- components/UserOperationManager.tsx
+++ components/UserOperationManager.tsx
@@ -1,8 +1,7 @@
-import { Tab } from "@headlessui/react";
 import Mint from "./Mint";
 import Burn from "./Burn";
 import React from "react";
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 import { ethers } from "ethers";
 import { useContractRead, useAccount } from "wagmi";
 import { FOLD_ADDRESS, XFOLD_ADDRESS } from "../const/contracts";
@@ -10,6 +9,7 @@ import { FOLD_ABI } from "../const/foldabi";
 import { XFOLD_ABI } from "../const/xfoldabi";
 import ApproveFold from "./ApproveFold";
 import ApproveXFold from "./ApproveXFold";
+import { SelectedTabContext } from "./Tabs";
 
 interface IUserInputContext {
   UserInput: string;
@@ -19,6 +19,7 @@ interface IUserInputContext {
 export const UserInputContext = createContext<IUserInputContext>(null!);
 
 export default function OperationManager() {
+  const { SelectedTab, setSelectedTab } = useContext(SelectedTabContext);
   const { address: USER_ADDRESS } = useAccount();
   const [UserInput, setUserInput] = useState();
   const { data: FoldApprovalAmount } = useContractRead({
@@ -44,35 +45,20 @@ export default function OperationManager() {
   }
 
   return (
-    <Tab.Group>
-      <Tab.List className="p-1 grid grid-cols-2 gap-4 items-center pb-2 text-white ">
-        <Tab className=" text-slate-300 text-lg font-light cursor-pointer hover:bg-fresh focus:outline-none aria-selected:bg-fresh rounded-[14px] border border-fresh p-3">
-          Stake
-        </Tab>
-        <Tab className="text-slate-300 text-lg font-light cursor-pointer hover:bg-fresh focus:outline-none aria-selected:bg-fresh rounded-[14px] border border-fresh p-3">
-          Withdraw
-        </Tab>
-      </Tab.List>
-      <Tab.Panels className="p-2">
-        <UserInputContext.Provider value={{ UserInput, setUserInput }}>
-          <Tab.Panel>
-            {parseFloat(USER_INPUT_AMOUNT) <=
-            parseFloat(FOLD_APPROVAL_AMOUNT) ? (
-              <Mint />
-            ) : (
-              <ApproveFold />
-            )}
-          </Tab.Panel>
-          <Tab.Panel>
-            {parseFloat(USER_INPUT_AMOUNT) <=
-            parseFloat(XFOLD_APPROVAL_AMOUNT) ? (
-              <Burn />
-            ) : (
-              <ApproveXFold />
-            )}
-          </Tab.Panel>
-        </UserInputContext.Provider>
-      </Tab.Panels>
-    </Tab.Group>
+    <>
+      <UserInputContext.Provider value={{ UserInput, setUserInput }}>
+        {SelectedTab == 0 ? (
+          parseFloat(USER_INPUT_AMOUNT) <= parseFloat(FOLD_APPROVAL_AMOUNT) ? (
+            <Mint />
+          ) : (
+            <ApproveFold />
+          )
+        ) : parseFloat(USER_INPUT_AMOUNT) <= parseFloat(XFOLD_APPROVAL_AMOUNT) ? (
+          <Burn />
+        ) : (
+          <ApproveXFold />
+        )}
+      </UserInputContext.Provider>
+    </>
   );
 }
